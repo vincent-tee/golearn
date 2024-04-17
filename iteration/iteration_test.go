9bf75e0b389e9c9b86004efd73427192be2e1e30
Vincent Tee <vincent.teeym@gmail.com>
2024-04-17 17:04:16 +1000

    added repeat function

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,12 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, &repeated)
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 3)
+	fmt.Println(repeated)
+	// Output: aaa
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
 }
