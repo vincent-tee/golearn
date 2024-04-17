3cd8422056cf0feb129248985a2417b6ba5bf6a0
Vincent Tee <vincent.teeym@gmail.com>
2024-04-17 18:17:19 +1000

    refactored to use collection

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,10 +1,14 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	// for i := 0; i < 5; i++ {
+	// 	sum += numbers[i]
+	// }
+
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
