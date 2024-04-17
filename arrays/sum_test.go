16a47d725ee30278b9f86175daca5536ab13f857
Vincent Tee <vincent.teeym@gmail.com>
2024-04-17 18:35:45 +1000

    added sumall and sumall tests

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -21,6 +24,27 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	t.Run("two collections", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("two collections", func(t *testing.T) {
+		got := SumAll([]int{1, 1, 1})
+		want := []int{3}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
