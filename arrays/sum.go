7f9302bf6e13b1f6fa1202730ba2b5a01355b065
Vincent Tee <vincent.teeym@gmail.com>
2024-04-18 12:08:22 +1000

    sum page finished

--- arrays/sum.go
+++ arrays/sum.go
@@ -23,3 +23,17 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+
+	return sums
+}
